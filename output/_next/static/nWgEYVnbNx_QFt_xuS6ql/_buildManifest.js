a84ff988fee262b020d185880b826468d3aff4af
hwang26 <hwang26@users.noreply.github.com>
2022-03-18 13:12:34 +0000

    Automated publish

--- output/_next/static/nWgEYVnbNx_QFt_xuS6ql/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/faad4584a532c157.css","static/chunks/pages/index-6e59f6999a5f29ba.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
