f6e6a631e3b1f9e1037e68cedc12b2f64d63f641
Hunter <hunterzwang@gmail.com>
2022-03-21 08:37:50 -0400

    Fixing config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/image-classifier/', 
+  basePath: '/image-classifier', 
   assetPrefix: '/image-classifier/' 
 }
 
