ee73fee18f877eedbeb79aa190806c9906645f0f
Hunter <hunterzwang@gmail.com>
2022-03-22 07:57:23 -0400

    Updated config file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  /*
   basePath: '/image-classifier', 
   assetPrefix: '/image-classifier' 
-  */
 }
 
 module.exports = nextConfig
