bda94861472300bd05a8060db08bc60f40bfd737
hwang26 <hwang26@users.noreply.github.com>
2022-03-22 11:55:18 +0000

    Automated publish

--- output/_next/static/2-wBI_TpwQoUbaxPkCYPx/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/faad4584a532c157.css","static/chunks/pages/index-089fdbcc34d6048e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
