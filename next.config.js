4da7c2eaca206178bf8f1002c643f1c1fd337ac3
Hunter <hunterzwang@gmail.com>
2022-03-23 08:35:34 -0400

    Merging

--- next.config.js
+++ next.config.js
@@ -2,10 +2,14 @@
 
 const isProd = process.env.NEXT_PUBLIC_ENVIRONMENT === "PROD";
 
+console.log("Test");
+console.log(isProd);
+console.log(process.env.NEXT_PUBLIC_ENVIRONMENT);
+
 const nextConfig = {
   reactStrictMode: true,
   basePath: isProd ? '/image-classifier' : '', 
-  assetPrefix: isProd ? '/image-classifier' : '', 
+  assetPrefix: isProd ? '/image-classifier' : '' 
 }
 
 module.exports = nextConfig
