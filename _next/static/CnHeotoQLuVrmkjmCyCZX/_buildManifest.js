aeb20a7c5a4e66c4e42755dd792dc3e2773b9fbb
hwang26 <hunterzwang@gmail.com>
2022-03-23 12:37:42 +0000

    Deploying to gh-pages from @ hwang26/image-classifier@4da7c2eaca206178bf8f1002c643f1c1fd337ac3 🚀

---
+++ _next/static/CnHeotoQLuVrmkjmCyCZX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b08f9b045131eb7e.css","static/chunks/pages/index-d44cdea4e5813df6.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
